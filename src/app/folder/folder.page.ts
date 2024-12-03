f43ce22c8e37139965b11bf0209a9c072cdc060f
gilcelioramos <gilcelioramos55@gmail.com>
2024-12-03 15:09:51 -0300

    subindo ajustes para consertar o erro

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,53 +1,36 @@
-// import { Component, inject, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import { AuthService } from '../services/auth.service';
-
-// @Component({
-//   selector: 'app-folder',
-//   templateUrl: './folder.page.html',
-//   styleUrls: ['./folder.page.scss'],
-// })
-// export class FolderPage {
-//   loginForm: FormGroup;
-  
-//   constructor(private fb: FormBuilder,private apiService: AuthService, private router: Router ) {
-//     this.loginForm = this.fb.group({
-//       cpf: ['', Validators.required],
-//       senha: ['', Validators.required], 
-//     });
-//   }
-
-//   login(){
-//     if (this.loginForm.valid) {
-//       const { cpf, senha } = this.loginForm.value;
-//       this.apiService.login(cpf, senha).subscribe(
-//         (response) => {
-//           console.log('Login bem-sucedido:', response);
-//           this.router.navigate(['/home']); // Redireciona após o login
-//         },
-//         (error) => {
-//           console.error('Erro no login:', error);
-//           alert('Credenciais inválidas!');
-//         }
-//       );
-//     }
-//   }
-// }
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
-export class FolderPage implements OnInit {
-  public folder!: string;
-  private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+export class FolderPage {
+  loginForm: FormGroup;
+  
+  constructor(private fb: FormBuilder,private apiService: AuthService, private router: Router ) {
+    this.loginForm = this.fb.group({
+      cpf: ['', Validators.required],
+      senha: ['', Validators.required], 
+    });
+  }
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  login(){
+    if (this.loginForm.valid) {
+      const { cpf, senha } = this.loginForm.value;
+      this.apiService.login(cpf, senha).subscribe(
+        (response) => {
+          console.log('Login bem-sucedido:', response);
+          this.router.navigate(['/home']); // Redireciona após o login
+        },
+        (error) => {
+          console.error('Erro no login:', error);
+          alert('Credenciais inválidas!');
+        }
+      );
+    }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,19 +10,22 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-    login(cpf: string, senha: string): Observable<any> {
-      const body = {
-        cpf,
-        senha,
-      };
-      return this.http.post(`${this.apiUrl}/login`, body);
-    }
-
-    resetPassword(novaSenha: string, confirmacaoSenha: string){
-      const body = {
-        novaSenha,
-        confirmacaoSenha,
-      };
-      return this.http.post(`${this.apiUrl}/login`, body);
+  login(cpf: string, senha: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    const body = {
+      cpf,
+      senha,
     };
+    return this.http.post(`${this.apiUrl}/login`, body, { headers });
+  }
+
+  resetPassword(novaSenha: string, confirmacaoSenha: string) {
+    const body = {
+      novaSenha,
+      confirmacaoSenha,
+    };
+    return this.http.post(`${this.apiUrl}/login`, body);
+  };
 }
